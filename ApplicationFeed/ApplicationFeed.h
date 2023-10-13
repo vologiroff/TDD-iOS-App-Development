ed17d6d6e24f68e15c10a5d309898afe36002dd3
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-14 00:35:02 +0200

    Create ApplicationFeed framework

---
+++ ApplicationFeed/ApplicationFeed.h
@@ -0,0 +1,18 @@
+//
+//  ApplicationFeed.h
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 14.10.23..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ApplicationFeed.
+FOUNDATION_EXPORT double ApplicationFeedVersionNumber;
+
+//! Project version string for ApplicationFeed.
+FOUNDATION_EXPORT const unsigned char ApplicationFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApplicationFeed/PublicHeader.h>
+
+
