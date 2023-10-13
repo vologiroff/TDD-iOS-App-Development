cf60d8403d677c1d3d61ef80d09dd132f8b35c5a
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-14 01:03:10 +0200

    RemoteFeedLoader does not request data upon creation

---
+++ ApplicationFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  ApplicationFeedTests
+//
+//  Created by Kantemir Vologirov on 14.10.23..
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromUrl() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+}
