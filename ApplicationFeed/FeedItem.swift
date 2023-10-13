cc4726ddd71659e001e77d5b23c575dea25cb0e2
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-14 00:41:20 +0200

    Create the FeedItem model

---
+++ ApplicationFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 14.10.23..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
