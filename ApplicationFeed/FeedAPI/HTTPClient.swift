c951662cf6d3be8aeeb4d92af187f1e7ad2ed9f9
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-15 19:57:22 +0200

    Move HTTPClient to its own file

---
+++ ApplicationFeed/FeedAPI/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 15.10.23..
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
