89677cb47b7f28e6b940d294ce26218406fde9b6
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-16 01:21:27 +0200

    Remove the generic error type from the 'LoadFeedResult' to simplify type constraints

--- ApplicationFeed/FeedAPI/RemoteFeedLoader.swift
+++ ApplicationFeed/FeedAPI/RemoteFeedLoader.swift
@@ -16,7 +16,7 @@ public final class RemoteFeedLoader: FeedLoader {
         case invalidData
     }
     
-    public typealias Result = LoadFeedResult<Error>
+    public typealias Result = LoadFeedResult
     
     public init(url: URL, client: HTTPClient) {
         self.url = url
@@ -31,7 +31,7 @@ public final class RemoteFeedLoader: FeedLoader {
             case .success(let data, let response):
                 completion(FeedItemsMapper.map(data, from: response))
             case .failure:
-                completion(.failure(.connectivity))
+                completion(.failure(Error.connectivity))
             }
         }
     }
