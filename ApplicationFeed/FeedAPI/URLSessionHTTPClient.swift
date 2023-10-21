7dcd85afed682015b4f61eb32260e8244314959d
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-21 23:49:09 +0200

    Move 'URLSessionHTTPClient' to its own file in production

---
+++ ApplicationFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 21.10.23..
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error {
+                completion(.failure(error))
+            } else if let data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            }
+            else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
