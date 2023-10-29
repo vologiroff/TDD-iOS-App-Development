18162fd7eeca32afb87d63c887781a5ce0332c41
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-29 15:25:29 +0100

    Rename 'RemoteFeedLoaderTests' to reflect the use case it refers to (Load Feed From Remote)

--- ApplicationFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ ApplicationFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import ApplicationFeed
 
-final class RemoteFeedLoaderTests: XCTestCase {
+final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromUrl() {
         let (_, client) = makeSUT()
