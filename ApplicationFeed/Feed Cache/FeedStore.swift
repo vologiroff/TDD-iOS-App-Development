f338afcfe7c955b012a334fbb3e07794b01406c4
Kantemir Vologirov <volkant0778@ya.ru>
2023-10-31 00:12:55 +0100

    Move 'FeedStore' to its own file

---
+++ ApplicationFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 31.10.23..
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
