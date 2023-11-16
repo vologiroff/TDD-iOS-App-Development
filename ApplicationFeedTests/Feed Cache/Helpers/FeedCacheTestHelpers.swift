105c34020d75ce7f5b67b58c85316d3cccbe8455
Kantemir Vologirov <volkant0778@ya.ru>
2023-11-16 02:04:31 +0100

    Retrieval from empty cache delivers empty result

--- ApplicationFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ ApplicationFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -26,7 +26,7 @@ extension Date {
     }
     
     private var feedCacheMaxAgeInDays: Int {
-        return 7
+        return 7            
     }
     
     private func adding(days: Int) -> Date {
