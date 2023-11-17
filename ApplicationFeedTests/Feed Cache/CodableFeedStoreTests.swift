18f3347f879baa697a847938469e941a36593d3e
Kantemir Vologirov <volkant0778@ya.ru>
2023-11-17 03:40:51 +0100

    Replace concrete 'CodableFeedStore' dependency in tests with the 'FeedStore' protocol to make the tests more decoupled from the concrete production implementations (also proving we respect the Liskov Substitution Principle)

--- ApplicationFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ ApplicationFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -204,7 +204,7 @@ class CodableFeedStoreTests: XCTestCase {
     
     private func makeSUT(storeURL: URL? = nil,
                          file: StaticString = #file,
-                         line: UInt = #line) -> CodableFeedStore {
+                         line: UInt = #line) -> FeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
@@ -212,7 +212,7 @@ class CodableFeedStoreTests: XCTestCase {
     
     @discardableResult
     private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date),
-                        to sut: CodableFeedStore) -> Error? {
+                        to sut: FeedStore) -> Error? {
         let exp = expectation(description: "Wait for cache insertion")
         var insertionError: Error?
         sut.insert(cache.feed, timestamp: cache.timestamp) { receivedInsertionError in
@@ -234,7 +234,7 @@ class CodableFeedStoreTests: XCTestCase {
         return deletionError
     }
     
-    private func expect(_ sut: CodableFeedStore,
+    private func expect(_ sut: FeedStore,
                         toRetrieveTwice expectedResult: RetrieveCachedFeedResult,
                         file: StaticString = #file,
                         line: UInt = #line) {
@@ -242,7 +242,7 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: expectedResult)
     }
     
-    private func expect(_ sut: CodableFeedStore,
+    private func expect(_ sut: FeedStore,
                         toRetrieve expectedResult: RetrieveCachedFeedResult,
                         file: StaticString = #file,
                         line: UInt = #line) {
