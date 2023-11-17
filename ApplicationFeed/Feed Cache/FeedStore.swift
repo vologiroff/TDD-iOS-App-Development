2b7c2b7589eff5ec19bd20e738a6692acaa30e1c
Kantemir Vologirov <volkant0778@ya.ru>
2023-11-17 02:50:33 +0100

    Extract duplicate retrieve test code into a reusable helper method

--- ApplicationFeed/Feed Cache/FeedStore.swift
+++ ApplicationFeed/Feed Cache/FeedStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum RetrieCachedFeedResult {
+public enum RetrieveCachedFeedResult {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
     case failure(Error)
@@ -16,7 +16,7 @@ public enum RetrieCachedFeedResult {
 public protocol FeedStore {
     typealias DeletionCompletion = (Error?) -> Void
     typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrieCachedFeedResult) -> Void
+    typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
