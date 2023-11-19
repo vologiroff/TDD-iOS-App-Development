c5e06881205a89612a30c130f5f2c5c9a830da1e
Kantemir Vologirov <volkant0778@ya.ru>
2023-11-19 17:04:22 +0100

    Add `CoreDataFeedStore` data model

---
+++ ApplicationFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,23 @@
+//
+//  ManagedCache.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 19.11.23..
+//
+
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
