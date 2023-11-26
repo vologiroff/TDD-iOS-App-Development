e07f251e5c6815dd9ffed6e49cbf7cb400c9f489
Kantemir Vologirov <volkant0778@ya.ru>
2023-11-26 22:11:54 +0100

    Cancel image loading when image view is not visible anymore

--- ApplicationFeediOS/FeedViewController.swift
+++ ApplicationFeediOS/FeedViewController.swift
@@ -10,6 +10,7 @@ import ApplicationFeed
 
 public protocol FeedImageDataLoader {
     func loadImageData(from url: URL)
+    func cancelImageDataLoad(from url: URL)
 }
 
 public final class FeedViewController: UITableViewController {
@@ -65,5 +66,10 @@ public final class FeedViewController: UITableViewController {
         cell.descriptionLabel.text = cellModel.description
         imageLoader?.loadImageData(from: cellModel.url)
         return cell
+    }
+    
+    public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cellModel = tableModel[indexPath.row]
+        imageLoader?.cancelImageDataLoad(from: cellModel.url)
     }
 }
