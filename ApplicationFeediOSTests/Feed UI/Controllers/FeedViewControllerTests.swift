e51219440c82bae7bb3ce232b58d1c30184a641a
Kantemir Vologirov <volkant0778@ya.ru>
2023-12-04 02:36:02 +0100

    Reuse table view cells from storyboard configuration

--- ApplicationFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ ApplicationFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -242,9 +242,20 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
     }
     
+    func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+        let (sut, loader) = makeSUT()
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [makeImage()])
+        
+        let view = sut.simulateFeedImageViewNotVisible(at: 0)
+        loader.completeImageLoading(with: anyImageData())
+        
+        XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+    }
+    
     //MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, 
+    private func makeSUT(file: StaticString = #file,
                          line: UInt = #line
     ) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
@@ -254,7 +265,11 @@ final class FeedViewControllerTests: XCTestCase {
         return (sut, loader)
     }
     
-    private func makeImage(description: String? = nil, 
+    private func anyImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
+    }
+    
+    private func makeImage(description: String? = nil,
                            location: String? = nil,
                            url: URL = URL(string: "http://any-url.com")!
     ) -> FeedImage {
