22e7b3c6e60ae791f48c3203b69f75871e2ee16d
Kantemir Vologirov <volkant0778@ya.ru>
2023-12-24 18:00:01 +0100

    Move FeedImageViewModel to separate file

--- ApplicationFeed/Feed Presentation/FeedImagePresenter.swift
+++ ApplicationFeed/Feed Presentation/FeedImagePresenter.swift
@@ -5,18 +5,6 @@
 //  Created by Kantemir Vologirov on 24.12.23..
 //
 
-public struct FeedImageViewModel<Image> {
-    public let description: String?
-    public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
-    
-    public var hasLocation: Bool {
-        return location != nil
-    }
-}
-
 public protocol FeedImageView {
     associatedtype Image
     
