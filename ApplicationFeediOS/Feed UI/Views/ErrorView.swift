617ec77a7a9aafc287296a88f82a759860863653
Kantemir Vologirov <volkant0778@ya.ru>
2023-12-24 01:59:04 +0100

    Move ErrorView to separate file

---
+++ ApplicationFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  ApplicationFeediOS
+//
+//  Created by Kantemir Vologirov on 24.12.23..
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
