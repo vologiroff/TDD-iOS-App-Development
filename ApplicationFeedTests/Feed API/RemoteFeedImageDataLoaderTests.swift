424fa69aa9d58f33f762aaca6f561153e9ed2c37
Kantemir Vologirov <volkant0778@ya.ru>
2024-01-03 01:59:56 +0300

    RemoteFeedImageDataLoader requests data from URL on `loadImageDataFromURL`

--- ApplicationFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ ApplicationFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -9,8 +9,14 @@ import XCTest
 import ApplicationFeed
 
 class RemoteFeedImageDataLoader {
-    init(client: Any) {
-
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (Any) -> Void) {
+        client.get(from: url) { _ in }
     }
 }
 
@@ -18,8 +24,17 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
 
     func test_init_doesNotPerformAnyURLRequest() {
         let (_, client) = makeSUT()
-
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 
     private func makeSUT(url: URL = anyURL(), 
@@ -32,8 +47,12 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         trackForMemoryLeaks(client, file: file, line: line)
         return (sut, client)
     }
-
-    private class HTTPClientSpy {
+    
+    private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            requestedURLs.append(url)
+        }
     }
 }
