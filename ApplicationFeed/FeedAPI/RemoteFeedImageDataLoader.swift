cd8e6b65db7c1eb48f9cd551dd220869bb44c273
Kantemir Vologirov <volkant0778@ya.ru>
2024-01-03 23:14:32 +0300

    Make RemoteFeedImageDataLoader conform to FeedImageDataLoader

--- ApplicationFeed/FeedAPI/RemoteFeedImageDataLoader.swift
+++ ApplicationFeed/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -5,7 +5,7 @@
 //  Created by Kantemir Vologirov on 3.1.24..
 //
 
-public final class RemoteFeedImageDataLoader {
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
     
     public init(client: HTTPClient) {
