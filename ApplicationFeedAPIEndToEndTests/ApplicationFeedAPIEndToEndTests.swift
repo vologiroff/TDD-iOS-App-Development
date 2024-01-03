0c26ac327b126d65584bba503de18f5a8d35e3e6
Kantemir Vologirov <volkant0778@ya.ru>
2024-01-03 23:34:08 +0300

    Add RemoteFeedImageDataLoader end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- ApplicationFeedAPIEndToEndTests/ApplicationFeedAPIEndToEndTests.swift
+++ ApplicationFeedAPIEndToEndTests/ApplicationFeedAPIEndToEndTests.swift
@@ -9,7 +9,7 @@ import XCTest
 import ApplicationFeed
 
 final class ApplicationFeedAPIEndToEndTests: XCTestCase {
-
+    
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         switch getFeedResult() {
         case let .success(imageFeed):
@@ -34,10 +34,24 @@ final class ApplicationFeedAPIEndToEndTests: XCTestCase {
         }
     }
     
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+        switch getFeedImageDataResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+            
+        case let .failure(error)?:
+            XCTFail("Expected successful image data result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful image data result, got no result instead")
+        }
+    }
+    
     // MARK: - Helpers
     
     private func getFeedResult(file: StaticString = #filePath,
-                               line: UInt = #line) -> FeedLoader.Result? {
+                               line: UInt = #line
+    ) -> FeedLoader.Result? {
         let testServerUrl = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerUrl, client: client)
@@ -56,55 +70,76 @@ final class ApplicationFeedAPIEndToEndTests: XCTestCase {
         return receivedResult
     }
     
+    private func getFeedImageDataResult(file: StaticString = #file, 
+                                        line: UInt = #line
+    ) -> FeedImageDataLoader.Result? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: FeedImageDataLoader.Result?
+        _ = loader.loadImageData(from: testServerURL) { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        return receivedResult
+    }
+    
     private func expectedImage(at index: Int) -> FeedImage {
-            return FeedImage(
-                id: id(at: index),
-                description: description(at: index),
-                location: location(at: index),
-                url: imageURL(at: index))
-        }
-
-        private func id(at index: Int) -> UUID {
-            return UUID(uuidString: [
-                "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
-                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
-                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
-                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-                "F79BD7F8-063F-46E2-8147-A67635C3BB01"
-            ][index])!
-        }
-
-        private func description(at index: Int) -> String? {
-            return [
-                "Description 1",
-                nil,
-                "Description 3",
-                nil,
-                "Description 5",
-                "Description 6",
-                "Description 7",
-                "Description 8"
-            ][index]
-        }
- 
-        private func location(at index: Int) -> String? {
-            return [
-                "Location 1",
-                "Location 2",
-                nil,
-                nil,
-                "Location 5",
-                "Location 6",
-                "Location 7",
-                "Location 8"
-            ][index]
-        }
-
-        private func imageURL(at index: Int) -> URL {
-            return URL(string: "https://url-\(index+1).com")!
-        }
-
+        return FeedImage(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            url: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+    
 }
