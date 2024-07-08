94f96c50862211734bd758b8cb6c346eebee0d55
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-09 02:03:50 +0300

    Remove unnecessary return statements

--- ApplicationFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ ApplicationFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -30,7 +30,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
     ) {
         perform { context in
             completion(Result {
-                return try ManagedFeedImage.first(with: url, in: context)?.data
+                try ManagedFeedImage.first(with: url, in: context)?.data
             })
         }
     }
