c49c5b70c7c1f29bf291935ef22318b2d97300d6
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-08 14:09:01 +0300

    Move CoreDataFeedStore to production

---
+++ ApplicationFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -0,0 +1,29 @@
+//
+//  CoreDataFeedStore+FeedImageDataStore.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 8.7.24..
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        perform { context in
+            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
+            
+            image.data = data
+            try? context.save()
+        }
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        perform { context in
+            completion(Result {
+                return try ManagedFeedImage.first(with: url, in: context)?.data
+            })
+        }
+    }
+    
+}
