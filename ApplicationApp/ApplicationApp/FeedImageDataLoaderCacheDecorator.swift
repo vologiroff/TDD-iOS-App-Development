45ded49948576dc5a2eb20ce14ee72a8a9b0e889
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-15 13:01:00 +0300

    Move FeedImageDataLoaderCacheDecorator to production

---
+++ ApplicationApp/ApplicationApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+//
+//  FeedImageDataLoaderCacheDecorator.swift
+//  ApplicationApp
+//
+//  Created by Kantemir Vologirov on 15.7.24..
+//
+
+import Foundation
+import ApplicationFeed
+
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            completion(result.map { data in
+                self?.cache.save(data, for: url) { _ in }
+                return data
+            })
+        }
+    }
+}
