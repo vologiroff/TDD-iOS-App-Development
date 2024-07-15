6c55b382aa917eacf39b21d1796272bb595281bf
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-15 12:24:53 +0300

    Move FeedCache to the Feed Feature production group

--- ApplicationApp/ApplicationAppTests/FeedLoaderCacheDecoratorTests.swift
+++ ApplicationApp/ApplicationAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -8,12 +8,6 @@
 import XCTest
 import ApplicationFeed
 
-protocol FeedCache {
-    typealias Result = Swift.Result<Void, Error>
-
-    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
-}
-
 final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
     private let cache: FeedCache
