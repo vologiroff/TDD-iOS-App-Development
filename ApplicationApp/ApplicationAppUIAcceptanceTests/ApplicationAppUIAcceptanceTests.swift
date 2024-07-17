c3628c8e7fb8838aacc46fdab2e091266d446149
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-17 10:07:56 +0300

    Add UI Test target for running high-level Acceptance Tests

---
+++ ApplicationApp/ApplicationAppUIAcceptanceTests/ApplicationAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationAppUIAcceptanceTests.swift
+//  ApplicationAppUIAcceptanceTests
+//
+//  Created by Kantemir Vologirov on 17.7.24..
+//
+
+import XCTest
+
+final class ApplicationAppUIAcceptanceTests: XCTestCase {
+
+   
+}
