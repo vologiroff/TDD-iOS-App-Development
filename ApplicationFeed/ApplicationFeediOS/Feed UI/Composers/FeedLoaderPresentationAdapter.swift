8ab85773c3b66789b2d5f19e7dc799dd10107528
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-22 19:30:00 +0300

    Replace FeedLoader composition with Combine operators

--- ApplicationFeed/ApplicationFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
+++ ApplicationFeed/ApplicationFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -5,28 +5,33 @@
 //  Created by Kantemir Vologirov on 10.12.23..
 //
 
+import Combine
 import ApplicationFeed
 import ApplicationFeediOS
 
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
+    private let feedLoader: () -> FeedLoader.Publisher
+    private var cancellable: Cancellable?
     var presenter: FeedPresenter?
     
-    init(feedLoader: FeedLoader) {
+    init(feedLoader: @escaping () -> FeedLoader.Publisher) {
         self.feedLoader = feedLoader
     }
     
     func didRequestFeedRefresh() {
         presenter?.didStartLoadingFeed()
         
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
+        cancellable = feedLoader().sink(
+            receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .finished: break
+                    
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingFeed(with: error)
+                }
+            }, receiveValue: { [weak self] feed in
                 self?.presenter?.didFinishLoadingFeed(with: feed)
-                
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
             }
-        }
+        )
     }
 }
