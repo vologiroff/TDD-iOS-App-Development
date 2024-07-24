4646d21b6030091182aa5ebc753ef9f0b0ac2cdf
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-24 23:55:45 +0300

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ ApplicationFeed/ApplicationFeed/FeedAPI/ImageCommentsMapper.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsMapper.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 24.7.24..
+//
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+
+        return root.items
+    }
+}
