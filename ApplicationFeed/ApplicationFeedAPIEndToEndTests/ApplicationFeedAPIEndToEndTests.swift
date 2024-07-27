d8e998dee85a7ca094783ec4587e70061252119c
Kantemir Vologirov <volkant0778@ya.ru>
2024-07-28 00:28:40 +0300

    Remove FeedLoader protocol as we don't need it anymore - we're composing the types with universal abstractions provided by the Combine framework

--- ApplicationFeed/ApplicationFeedAPIEndToEndTests/ApplicationFeedAPIEndToEndTests.swift
+++ ApplicationFeed/ApplicationFeedAPIEndToEndTests/ApplicationFeedAPIEndToEndTests.swift
@@ -51,11 +51,11 @@ final class ApplicationFeedAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #filePath,
                                line: UInt = #line
-    ) -> FeedLoader.Result? {
+    ) -> Swift.Result<[FeedImage], Error>? {
         let client = ephemeralClient()
         let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: FeedLoader.Result?
+        var receivedResult: Swift.Result<[FeedImage], Error>?
         client.get(from: feedTestServerURL) { result in
             receivedResult = result.flatMap { (data, response) in
                 do {
