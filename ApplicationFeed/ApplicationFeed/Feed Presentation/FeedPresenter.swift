5a79ec525a18eaa6317481a6df04274296428d48
Kantemir Vologirov <volkant0778@ya.ru>
2024-08-02 01:27:47 +0300

    Remove unused FeedPresenter logic

--- ApplicationFeed/ApplicationFeed/Feed Presentation/FeedPresenter.swift
+++ ApplicationFeed/ApplicationFeed/Feed Presentation/FeedPresenter.swift
@@ -5,56 +5,15 @@
 //  Created by Kantemir Vologirov on 24.12.23..
 //
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
-    private let feedView: FeedView
-    private let loadingView: ResourceLoadingView
-    private let errorView: ResourceErrorView
-    
-    private var feedLoadError: String {
-        return NSLocalizedString(
-            "GENERIC_CONNECTION_ERROR",
-            tableName: "Shared",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Error message displayed when we can't load the image feed from the server"
-        )
-    }
     
     public static var title: String {
-        return NSLocalizedString(
+        NSLocalizedString(
             "FEED_VIEW_TITLE",
             tableName: "Feed",
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Title for the feed view"
         )
-    }
-    
-    public init(
-        feedView: FeedView,
-        loadingView: ResourceLoadingView,
-        errorView: ResourceErrorView
-    ) {
-        self.feedView = feedView
-        self.loadingView = loadingView
-        self.errorView = errorView
-    }
-    
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-
-    public func didFinishLoadingFeed(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
