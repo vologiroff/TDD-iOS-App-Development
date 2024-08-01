4ea1d925079fab36dd7aef76010951c1a2dde6ee
Kantemir Vologirov <volkant0778@ya.ru>
2024-08-01 23:32:51 +0300

    Remove duplication in the localization tests

--- ApplicationFeed/ApplicationFeed/Feed Presentation/FeedPresenter.swift
+++ ApplicationFeed/ApplicationFeed/Feed Presentation/FeedPresenter.swift
@@ -21,27 +21,31 @@ public final class FeedPresenter {
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
-
+    
     private var feedLoadError: String {
-        return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                tableName: "Shared",
-                bundle: Bundle(for: FeedPresenter.self),
-                comment: "Error message displayed when we can't load the image feed from the server")
+        return NSLocalizedString(
+            "GENERIC_CONNECTION_ERROR",
+            tableName: "Shared",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Error message displayed when we can't load the image feed from the server"
+        )
     }
-
+    
+    public static var title: String {
+        return NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "Title for the feed view"
+        )
+    }
+    
     public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
         self.feedView = feedView
         self.loadingView = loadingView
         self.errorView = errorView
     }
-
-    public static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
-            tableName: "Feed",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Title for the feed view")
-    }
-
+    
     public func didStartLoadingFeed() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
