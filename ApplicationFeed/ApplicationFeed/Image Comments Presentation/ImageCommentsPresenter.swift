3fc370caae3823bd57d22106f04e7d6dc2fc2f1c
Kantemir Vologirov <volkant0778@ya.ru>
2024-08-02 22:17:05 +0300

    Add image comments title

---
+++ ApplicationFeed/ApplicationFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCommentsPresenter.swift
+//  ApplicationFeed
+//
+//  Created by Kantemir Vologirov on 2.8.24..
+//
+
+public final class ImageCommentsPresenter {
+    public static var title: String {
+        NSLocalizedString(
+            "IMAGE_COMMENTS_VIEW_TITLE",
+            tableName: "ImageComments",
+            bundle: Bundle(for: Self.self),
+            comment: "Title for the image comments view"
+        )
+    }
+}
