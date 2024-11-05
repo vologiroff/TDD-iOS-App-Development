f17203c3e937c0adedc16c671d2930b2d647f408
Kantemir Vologirov <volkant0778@ya.ru>
2024-11-05 21:04:58 +0300

    Duplicate FeedUIComposer as CommentsUIComposer

--- ApplicationApp/ApplicationAppTests/FeedUIIntegrationTests.swift
+++ ApplicationApp/ApplicationAppTests/FeedUIIntegrationTests.swift
@@ -10,7 +10,7 @@ import UIKit
 import ApplicationFeed
 import ApplicationFeediOS
 
-final class FeedUIIntegrationTests: XCTestCase {
+class FeedUIIntegrationTests: XCTestCase {
     
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
