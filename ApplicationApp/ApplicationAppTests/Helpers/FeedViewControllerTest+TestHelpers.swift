0f923afd4b56dbe710636a9823847a22d5b182e9
Kantemir Vologirov <volkant0778@ya.ru>
2024-11-07 23:39:39 +0300

    Render comments

--- ApplicationApp/ApplicationAppTests/Helpers/FeedViewControllerTest+TestHelpers.swift
+++ ApplicationApp/ApplicationAppTests/Helpers/FeedViewControllerTest+TestHelpers.swift
@@ -11,14 +11,14 @@ import ApplicationFeediOS
 extension ListViewController {
     public override func loadViewIfNeeded() {
         super.loadViewIfNeeded()
-
+        
         tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
     }
     
     var isShowingLoadingIndicator: Bool {
         return refreshControl?.isRefreshing == true
     }
-
+    
     var errorMessage: String? {
         return errorView.message
     }
@@ -26,6 +26,40 @@ extension ListViewController {
     private var feedImagesSection: Int {
         return 0
     }
+}
+
+extension ListViewController {
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 :  tableView.numberOfRows(inSection: commentsSection)
+    }
+    
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: commentsSection)
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+    
+    private var commentsSection: Int {
+        return 0
+    }
+}
+
+extension ListViewController {
     
     @discardableResult
     func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
