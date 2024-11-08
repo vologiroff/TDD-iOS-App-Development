e89bec2c4f9166b335d3c0bdb9b5551dc9d75fce
Kantemir Vologirov <volkant0778@ya.ru>
2024-11-09 01:20:56 +0300

    Remove feed references from comments integration tests

--- ApplicationApp/ApplicationAppTests/CommentsUIIntegrationTests.swift
+++ ApplicationApp/ApplicationAppTests/CommentsUIIntegrationTests.swift
@@ -12,7 +12,7 @@ import ApplicationApp
 import ApplicationFeed
 import ApplicationFeediOS
 
-class CommentsUIIntegrationTests: FeedUIIntegrationTests {
+class CommentsUIIntegrationTests: XCTestCase {
 
     func test_commentsView_hasTitle() {
         let (sut, _) = makeSUT()
@@ -36,7 +36,7 @@ class CommentsUIIntegrationTests: FeedUIIntegrationTests {
         XCTAssertEqual(loader.loadCommentsCallCount, 3, "Expected yet another loading request once user initiates another reload")
     }
 
-    override func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
+    func test_loadingCommentsIndicator_isVisibleWhileLoadingComments() {
         let (sut, loader) = makeSUT()
 
         sut.simulateAppearance()
@@ -106,7 +106,7 @@ class CommentsUIIntegrationTests: FeedUIIntegrationTests {
         wait(for: [exp], timeout: 1.0)
     }
 
-    override func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+    func test_loadCommentsCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
 
         sut.simulateAppearance()
