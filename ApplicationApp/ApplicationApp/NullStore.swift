c7f38e58416c62264d3d81b1cb2e6c3cbc755caf
Kantemir Vologirov <volkant0778@ya.ru>
2024-12-14 23:47:40 +0300

    Use NullStore when we can't create a CoreDataFeedStore instance

---
+++ ApplicationApp/ApplicationApp/NullStore.swift
@@ -0,0 +1,31 @@
+//
+//  NullStore.swift
+//  ApplicationApp
+//
+//  Created by Kantemir Vologirov on 14.12.24..
+//
+
+import Foundation
+import ApplicationFeed
+
+class NullStore: FeedStore & FeedImageDataStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+    }
+
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        completion(.success(()))
+    }
+
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+}
