b66370f2edc71b21d5f2b5122f173efd3d440477
Kantemir Vologirov <volkant0778@ya.ru>
2024-12-21 22:52:29 +0300

    Rename method to clarify intent

--- ApplicationFeed/ApplicationFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ ApplicationFeed/ApplicationFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -14,7 +14,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
         for url: URL,
         completion: @escaping (FeedImageDataStore.InsertionResult) -> Void
     ) {
-        perform { context in
+        performAsync { context in
             completion (Result {
                 try ManagedFeedImage.first(with: url, in: context)
                     .map { $0.data = data }
@@ -27,7 +27,7 @@ extension CoreDataFeedStore: FeedImageDataStore {
         dataForURL url: URL,
         completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void
     ) {
-        perform { context in
+        performAsync { context in
             completion(Result {
                 try ManagedFeedImage.data(with: url, in: context)
             })
