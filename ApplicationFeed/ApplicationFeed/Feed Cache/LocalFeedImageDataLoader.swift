3f1afd5a82049694ab8d429dc759640d509d6c65
Kantemir Vologirov <volkant0778@ya.ru>
2024-12-21 22:51:27 +0300

    Make FeedImageDataLoader sync

--- ApplicationFeed/ApplicationFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ ApplicationFeed/ApplicationFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -30,48 +30,20 @@ extension LocalFeedImageDataLoader: FeedImageDataCache {
 }
     
 extension LocalFeedImageDataLoader: FeedImageDataLoader {
-    public typealias LoadResult = FeedImageDataLoader.Result
-    
     public enum LoadError: Error {
         case failed
         case notFound
     }
 
-    private final class LoadImageDataTask: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-
-        func cancel() {
-            preventFurtherCompletions()
-        }
-
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-
-    public func loadImageData(
-        from url: URL,
-        completion: @escaping (LoadResult) -> Void
-    ) -> FeedImageDataLoaderTask {
-        let task = LoadImageDataTask(completion)
-
-        task.complete(
-            with: Swift.Result {
-                try store.retrieve(dataForURL: url)
+    public func loadImageData(from url: URL) throws -> Data {
+        do {
+            if let imageData = try store.retrieve(dataForURL: url) {
+                return imageData
             }
-            .mapError { _ in LoadError.failed }
-            .flatMap { data in
-                data.map { .success($0) } ?? .failure(LoadError.notFound)
-            })
+        } catch {
+            throw LoadError.failed
+        }
 
-        return task
+        throw LoadError.notFound
     }
 }
